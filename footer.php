10a53cb7fc5ce9efece8dda10538a17718d817d2
Jay-snow <marcusjsnow1@gmail.com>
2019-05-14 16:00:15 -0400

    first real commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      This website was created by Marcus Snow. 
+    </p>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
