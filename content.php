49aeb9a8125bf392802ab4a8dbda657ccd66ecb5
Jay-snow <marcusjsnow1@gmail.com>
2019-05-22 11:50:31 -0400

    Cleaned up code, need to make footer sticky.

--- content.php
+++ content.php
@@ -5,36 +5,57 @@
 a {
   color:black;
 }
+
+
+.card {
+  height:100%;
+  display: flex;
+  flex-direction:column;
+}
+
+.card-content {
+  flex: auto;
+}
+
+.card-footer {
+  margin-top: auto;
+}
 </style>
 
 
 <!-- Blog post excerpt -->
 <div class="card">
   <div class="columns is-gapless">
-  <div class="column is-one-quarter">
+  <div class="column is-one-third">
      <div class="card-image">
-      <figure class="image is-184x184">
-        <img src="https://bulma.io/images/placeholders/128x128.png">
+      <figure class="image is-1by1">
+      <a href="https://placeholder.com"><img src="https://via.placeholder.com/184"></a>
+       
       </figure>
     </div>
   </div>
 
   <div class="column">
-    <div class="blog-post" style="background-color:white;border:1px solid;">
-      <div class="content">     
+    
+      <div class="card-content ">     
         <!-- Blog post title -->
          <h1> <a href="<?php the_permalink(); ?> "> <?php the_title(); ?> </a> </h1>
           
         <!-- Blog post excerpt -->
         <p> <?php the_excerpt(); ?> </p>
-        
-        <!-- Blog post author -->
-        <footer class="card-footer">
-        <p> By <?php the_author(); ?> </p>
-        </footer>
+      
 
       </div>
-    </div>
+
+    
+
+          <!-- Blog post author -->
+          <footer class="card-footer ">
+       <p class="card-footer-item">  By <?php the_author(); ?>  </p>
+         
+        </footer>
+
+
   </div> <!-- end of column -->
   </div> <!-- end of columns -->
 </div> <!-- end of card -->
